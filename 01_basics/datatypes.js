ae24dcc9efe513e8c56aba0beb5bf7de0340c02a
smaraktripathy <smaraktripathy94@gmail.com>
2026-08-22 21:29:00 +0000

    add javascript basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+ "use strict"; // use all in new js model
+//  alert("smarak hello") we are using nodejs
+console.log(9 + 1)
+let name= "smarak"
+let age=22
+let isloggedId= true
+// null stand alone value
+// symbol
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined type
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const acctId=12345
+let acctemail="samraktripathy@gmail.cum"
+var acctpass="khachudu94"
+acctcity="dareikela"
+var acctstate;
+/*
+prefer to not use var
+*/
+// acctId=2
+console.log(acctId);
+acctemail="fbwlibfwif"
+console.table([acctId,acctemail,acctpass,acctcity,acctstate])
