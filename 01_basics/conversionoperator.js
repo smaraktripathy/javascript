ab4a07bc23bf5cca9d49e862686a19662101e9f9
Smarak Tripathy <smaraktripathy94@gmail.com>
2026-08-24 01:38:57 +0530

    conversion complete

---
+++ 01_basics/conversionoperator.js
@@ -0,0 +1,18 @@
+let score=false;
+// console.log(typeof score);
+// console.log(typeof (score));
+let valuenum= Number(score);
+// console.log(typeof valuenum );
+// console.log(valuenum );
+let login= "smarak";
+let checklogin=Boolean(login);
+// console.log(checklogin);
+// ****** operation* *******
+let num=2;
+let negnum=-num;
+// console.log(negnum);
+// console.log(2+3);
+// console.log(2**3);
+let x=4;
+const y=x++;
+console.log(`x:${x},y:${y}`)
