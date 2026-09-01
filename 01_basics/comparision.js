1bc22d8afc7ec9c19464d16a5ec99d2294500456
smaraktripathy <smaraktripathy94@gmail.com>
2026-09-01 19:24:41 +0000

    sept1

---
+++ 01_basics/comparision.js
@@ -0,0 +1,7 @@
+// we shoouldn't use this type of comparision
+
+// console.log(null>=0)
+// console.log(null==0)
+// console.log(null>0)
+// console.log(undefined==0)
+console.log("2"===2);
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,34 @@
+//  primitive(call by value)
+// 7 types-number,boolean,string,null,undefined,BigInt,symbol
+
+// const name="smarak"
+// const score=100
+// const scorevalue=100.23
+// console.log(score)
+
+
+// const id= Symbol("pussy")
+// const idsecond= Symbol("pussy")
+// console.log(id===idsecond);
+
+// ARRAY
+const arry=["ironman","captain america","thor"];
+
+// OBJECT
+let obj={
+    name:"smarak",
+    class:12,
+    sex:"male"
+}
+
+// FUNCTION
+const myfuncton = function(){
+   console.log("heelo world");
+   
+}
+const num= 1000000000000000n
+
+console.log( typeof num);
+
+// non primitive(reference)
+// arrays , objects,functions
